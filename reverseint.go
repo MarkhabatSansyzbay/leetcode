6335441333f7b1e191cc9c8f9406be6a36a0a9d0
Markhabat <msansyzbay@gmail.com>
2022-04-09 22:37:24 +0600

    task #7. Function to reverse an integer number by digits.

---
+++ reverseint.go
@@ -0,0 +1,27 @@
+package myFunctions
+
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+func reverse(x int) int {
+	sign := 1
+	if x < 0 {
+		sign = -1
+		x *= -1
+	}
+
+	var digits []int
+	for x > 0 {
+		digits = append(digits, x%10)
+		x /= 10
+	}
+
+	num := 0
+	for i := 0; i < len(digits); i++ {
+		if num > 214748364 || num > 214748364 && digits[i] > 7 {
+			return 0
+		}
+		num = num*10 + digits[i]
+	}
+
+	return num * sign
+}
