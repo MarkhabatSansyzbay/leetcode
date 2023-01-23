51cc22274887422fd37877d2cdba9bea3ea7f0a8
Markhabat <msansyzbay@gmail.com>
2023-01-23 18:32:56 +0600

    #283 easy

---
+++ move_zeroes.go
@@ -0,0 +1,18 @@
+package myFunctions
+
+// 283. Move Zeroes
+
+func moveZeroes(nums []int) {
+	count := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[count] = nums[i]
+			count++
+		}
+	}
+
+	for i := count; i < len(nums); i++ {
+		nums[i] = 0
+	}
+}
